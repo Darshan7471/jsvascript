0e684d2908bab9ad87191014bcaf2702703e94d8
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-08-19 11:42:52 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
 console.log('darshan');
+
+
+const accountId = 144553
+
+let accounEmail = 'darshan@hmail.com'
+
+var accountPass = "12345"
+
+accountcity = "Nashik"
+
+accountId = 2
+
+console.log(accountId);
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+console.log('darshan');
+
+
+const accountId = 144553
+let accountEmail = 'darshan@hmail.com'
+var accountPass = "12345"
+accountcity = "Nashik"
+
+// accountId = 2 //not allowed
+
+accounEmail = "darshan8055@gmail.com"
+accountPass = "1234"
+accountcity = "dubai"
+
+let accounState;
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPass, accountcity, accounState])
+
+// prefrer not to use var
+
+// because of issues in block scope and functions scope
