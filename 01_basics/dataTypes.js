41d41fc677f4d964e355ce08d3201b677c7de168
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-08-19 12:45:51 +0000

    datatypes in js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") we are using node js not browser
+
+let name = "darshan"
+let age = 24
+let isLoggedIn = false
+
+
+// premitive data types
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique
+
+// Object
+
+console.log(typeof "darshan")
+
