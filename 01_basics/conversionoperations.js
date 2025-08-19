528c2e7e896535b7398e6f67e3107d66c6381b67
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-08-19 16:57:47 +0000

    conversions of datatypes

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,35 @@
+// let score = "33"
+// let score = "33abc"
+let score = false
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber); // it will print NaN (Not a number)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isloggedIn = "darshan"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+
+console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let Stringnumber = String(someNumber)
+
+console.log(Stringnumber);
+console.log(typeof (Stringnumber));
+
