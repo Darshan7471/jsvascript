78ffd7846e9d03f7858c9333c5d83c61528516a1
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-08-20 13:11:46 +0000

    arrayys

---
+++ 02_basics/01-arrays.js
@@ -0,0 +1,17 @@
+// array
+
+const myArray = [0, 1,2,3,4,5]
+
+const myHeros = ["shaktiman", "Gangadhar"]
+
+const myArr2 = new Array (1,2,3,4)
+console.log(myArray[0])
+
+// Array Methods
+
+// myArray.push(6)
+// myArray.pop()
+
+myArray.unshift(9)
+
+console.log(myArray)
