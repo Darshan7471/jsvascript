efbfa7013f8ad396d50108fdafc509e4f0a8fda4
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-08-20 11:11:01 +0000

    math and num

--- 01_basics/nums_and_math.js
+++ 01_basics/nums_and_math.js
@@ -28,5 +28,9 @@
 // console.log(Math.max(4,3,8,9));
 
 console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
 
+const min = 10
+const max = 20
 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
